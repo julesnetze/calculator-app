0709cde8af53b6fed146ab0d63cbfb18bec1111c
julesnetze <julesnetze@gmail.com>
2020-11-20 16:56:42 +0100

    Refactor Application class and add stop function

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,18 +1,30 @@
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status
 import org.http4k.core.Status.Companion.OK
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
 class Application {
-
-    private val app = { _: Request ->
+    private val router = { _: Request ->
         Response(OK)
     }
 
-    fun main() {
-        app.asServer(SunHttp(9000)).start()
+    private val server = router.asServer(SunHttp(9000))
+
+    fun start() {
+        server.start()
     }
 
+    fun stop() {
+        server.stop()
+    }
+}
+
+val app = Application()
+fun main() {
+    app.start()
+}
+
+fun stop() {
+    app.stop()
 }
--- src/test/kotlin/ApiTest.kt
+++ src/test/kotlin/ApiTest.kt
@@ -2,22 +2,20 @@ import org.http4k.client.ApacheClient
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class ApiTest {
-
     @Test
     fun `server should be up`() {
+        main()
         val client = ApacheClient()
-        val server = Application()
-        server.main()
 
         val request = Request(GET, "http://localhost:9000")
 
         val response: Response = client(request)
-
-        assertEquals(Status.OK, response.status)
+        assertEquals(OK, response.status)
+        stop()
     }
 }
